784a95e547718c1c0620e7fcdb2e1397edb1f066
x.martin <xavier.martin@itsfactory.fr>
2020-04-05 17:37:01 +0200

    add to do and docs

--- srcs/render/launch_render.c
+++ srcs/render/launch_render.c
@@ -6,7 +6,7 @@
 /*   By: xamartin <xamartin@student.le-101.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/04 22:36:23 by xamartin          #+#    #+#             */
-/*   Updated: 2020/04/05 16:35:21 by xamartin         ###   ########lyon.fr   */
+/*   Updated: 2020/04/05 17:36:51 by xamartin         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 /*
 Need function to use:	- new window -> OK 
-						- clear window
-						- put pixel
+						- clear window -> glClear
+						- put pixel -> glDrawPixel but not optim maybe create a texture see link 1
 						- new image
 						- get data addr
 						- put image to window
@@ -27,6 +27,30 @@ Need function to use:	- new window -> OK
 						- destroy image
 						- swap buffer -> flushGLContext
 						- change context ->  selectGLContext
+
+						
+1 - http://www.multigesture.net/articles/how-to-draw-pixels-to-a-texture-opengl/
+
+
+TO DO:
+		- put pixel (char-RGB) in a texture of the size of a screen
+		- draw lines
+		- load img (xpm free to use else code it or change imaeg type to xpm)
+		- create image (load image and put it in a texture)
+		- multi screen in windows
+		- change screen (context)
+
+		MATRIX:
+		- create a lib to computate matrix
+		- mutliply
+		- add
+		- pow
+
+		MENU: 
+		- create rect | circle | fonts -> for menus
+		- create hit box for button and other things
+
+add things when we things about it
 
 */
 
