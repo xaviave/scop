811c68d48f35b90f2a4f6e0867ce0e329593baab
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-06-18 23:41:20 +0200

    Norm.

--- includes/error.h
+++ includes/error.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   error.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: xamartin <xamartin@student.le-101.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/12 11:03:30 by xamartin          #+#    #+#             */
-/*   Updated: 2020/04/16 15:57:02 by xamartin         ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef ERROR_H
 # define ERROR_H
 
@@ -20,7 +8,7 @@
 ** Functions
 */
 
-void        handle_error_parser(char *message, t_addr **addr);
-void		handle_error_render(char *message, t_addr **addr);
+void	handle_error_parser(char *message, t_addr **addr);
+void	handle_error_render(char *message, t_addr **addr);
 
 #endif
