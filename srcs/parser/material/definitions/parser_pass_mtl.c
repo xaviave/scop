94ff85de181a8bb66c803b99c5a5023186594a57
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-06-19 19:01:21 +0200

    Norm.

--- srcs/parser/material/definitions/parser_pass_mtl.c
+++ srcs/parser/material/definitions/parser_pass_mtl.c
@@ -6,7 +6,7 @@
 /*   By: ltoussai <lotoussa@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 19:00:11 by ltoussai          #+#    #+#             */
-/*   Updated: 2020/06/19 19:00:19 by ltoussai         ###   ########lyon.fr   */
+/*   Updated: 2020/06/19 19:01:20 by ltoussai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int					parser_pass_mtl(t_mtl *mtl, char *raw_data, int group_id)
 	}
 	else
 		printf("This line is ignored | mtl id = %d | data = %s\n",
-			   mtl->id, raw_data);
+			mtl->id, raw_data);
 	return (1);
 }
