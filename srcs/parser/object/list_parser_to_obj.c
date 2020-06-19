5ec87ffac6be39003f5ba5f17592e60013880a26
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-06-19 19:06:42 +0200

    Norm.

--- srcs/parser/object/list_parser_to_obj.c
+++ srcs/parser/object/list_parser_to_obj.c
@@ -6,7 +6,7 @@
 /*   By: ltoussai <lotoussa@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 19:02:05 by ltoussai          #+#    #+#             */
-/*   Updated: 2020/06/19 19:02:12 by ltoussai         ###   ########lyon.fr   */
+/*   Updated: 2020/06/19 19:06:33 by ltoussai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int					list_parser_to_obj(t_obj *obj, t_list_parser *list,
 	int				o_id;
 	int				g_id;
 	t_list_parser	*tmp;
-	int 			(*f[7])(t_obj *, char *, int, int);
+	int				(*f[7])(t_obj *, char *, int, int);
 
 	init_parser_obj_ptr(f);
 	o_id = -1;
@@ -43,16 +43,14 @@ int					list_parser_to_obj(t_obj *obj, t_list_parser *list,
 	{
 		if (tmp->id == ID_G || tmp->id == ID_O)
 		{
-			if (tmp->id == ID_O)
-				o_id = obj->len_objects;
-			else
-				g_id = obj->len_groups;
+			o_id = tmp->id == ID_O ? obj->len_objects : o_id;
+			g_id = tmp->id != ID_O ? obj->len_groups : g_id;
 			if (!(tmp->id == ID_G ?
 			parser_g(obj, tmp->data, o_id) : parser_o(obj, tmp->data)))
 				handle_error_parser("Error during memory allocation.", addr);
 		}
 		else if (tmp->id < ID_G && !(f[tmp->id](obj, tmp->data, o_id, g_id)))
-				handle_error_parser("Error during memory allocation.", addr);
+			handle_error_parser("Error during memory allocation.", addr);
 		tmp = tmp->next;
 	}
 	return (check_obj(obj));
