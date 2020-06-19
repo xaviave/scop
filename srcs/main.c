64c877e9bd0eba907a9c16e0ac0981cd54fce6e3
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-06-19 15:15:48 +0200

    Norm main.c

--- srcs/main.c
+++ srcs/main.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xamartin <xamartin@student.le-101.fr>      +#+  +:+       +#+        */
+/*   By: ltoussai <lotoussa@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/12 11:19:28 by xamartin          #+#    #+#             */
-/*   Updated: 2020/05/03 12:34:23 by xamartin         ###   ########lyon.fr   */
+/*   Created: 2020/06/19 15:11:07 by ltoussai          #+#    #+#             */
+/*   Updated: 2020/06/19 15:15:39 by ltoussai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 #include "render.h"
 
-int             main(int ac, char **av)
+int				main(int ac, char **av)
 {
-    t_parser    parser;
-    t_gdata      g;
+	t_parser	parser;
+	t_gdata		g;
 
 	launch_parser(&parser, ac, av);
 	launch_render(&g, &parser);
-    system("leaks scop");
-    return (0);
+	system("leaks scop");
+	return (0);
 }
