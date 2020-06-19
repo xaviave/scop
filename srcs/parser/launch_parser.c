60829c11480167c5a0fb6b0f3e60ba303f40aed0
Louis TOUSSAINT <louistoussaint98@gmail.com>
2020-06-19 17:35:09 +0200

    Norm.

--- srcs/parser/launch_parser.c
+++ srcs/parser/launch_parser.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   launch_parser.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xamartin <xamartin@student.le-101.fr>      +#+  +:+       +#+        */
+/*   By: ltoussai <lotoussa@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/12 11:23:44 by xamartin          #+#    #+#             */
-/*   Updated: 2020/05/03 12:33:53 by xamartin         ###   ########lyon.fr   */
+/*   Created: 2020/06/19 17:29:32 by ltoussai          #+#    #+#             */
+/*   Updated: 2020/06/19 17:35:08 by ltoussai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser.h"
+#include "../../includes/parser.h"
 
 /*
 ** check_args: check every args of the main
@@ -24,19 +24,21 @@ static void	check_args(int ac, char **av)
 	char	*tmp;
 
 	if (ac < 2)
-		handle_error_parser("No args provided, need at least one object file.", NULL);
-    i = 0;
+		handle_error_parser("No args provided, need at least one object file.",
+			NULL);
+	i = 0;
 	while (++i < ac)
 	{
-	    if (ft_strlen(av[i]) < 5)
-            handle_error_parser("Invalid file name.", NULL);
-		else if (!(tmp = ft_strsub(av[i], ft_strlen(av[i]) - 4, ft_strlen(av[i]))))
-            handle_error_parser("Error during memory allocation.", NULL);
+		if (ft_strlen(av[i]) < 5)
+			handle_error_parser("Invalid file name.", NULL);
+		else if (!(tmp = ft_strsub(av[i], ft_strlen(av[i]) - 4,
+				ft_strlen(av[i]))))
+			handle_error_parser("Error during memory allocation.", NULL);
 		else if (ft_strcmp(".obj", tmp))
 		{
-		    ft_strdel(&tmp);
-            handle_error_parser("Invalid file name.", NULL);
-        }
+			ft_strdel(&tmp);
+			handle_error_parser("Invalid file name.", NULL);
+		}
 		ft_strdel(&tmp);
 	}
 }
@@ -48,6 +50,6 @@ int 		launch_parser(t_parser *parser, int ac, char **av)
 	reader_obj(parser);
 	init_parser_mtl(parser);
 	reader_mtl(parser);
-    clear_parser_memory(&parser->addr);
+	clear_parser_memory(&parser->addr);
 	return (0);
 }
